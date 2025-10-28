3050b030fb37209869239621e306238258fdb784
ISHPREET1313 <ishpreetsingh13ips@gmail.com>
2025-10-28 14:30:00 +0530

    Lab

---
+++ Practical Lab/Questions/Q_35pointer2pointer.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int x=30;
+    int *p=&x;
+    int **q=&p;
+    printf("Address of x %p\n",&x);
+    printf("Address of p %p\n",&p);
+    // printf("Address of q %p\n\n",&q);
+
+    printf("value of x is %d\n",x);
+    printf("value of p is %p\n",p);
+    printf("value of *q is %p\n",*q);
+    printf("value of *p is %d\n",*p);
+    printf("value of **q is %d\n",**q);
+    printf("value of q is %p\n",q);
+    return 0;
+}
